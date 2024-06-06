83b71954086075c89633e6b4b122d9960f36c7e1
nradwin <111891092+nradwin@users.noreply.github.com>
2024-06-06 12:08:06 -0500

    Add files via upload

---
+++ Day 1/BandNameGenerator.py
@@ -0,0 +1,10 @@
+#1. Greeting
+print("Welcome to the Band Name Generator")
+#2. User's life contxt
+city_born = input("To what city were you born?\n")
+city_dev = input("In what city did you self-actualize?\n")
+pet_type = input("what animal did you choose to develop alongside?\n") 
+pet_name = input("What did you name it?\n")
+#4. Band name idea generator
+bandname = city_dev + pet_type
+print(bandname)
