ccc093b0ee244bf6b878045ce9bc1ac3700c1d86
nradwin <111891092+nradwin@users.noreply.github.com>
2024-06-15 14:27:58 -0500

    Update calculator.py

--- day010/calculator.py
+++ day010/calculator.py
@@ -1,9 +1,8 @@
 from calc_ascii import logo
-print(logo)
 
 # calculator
 
-# addition
+# add
 def add(n1, n2):
   return n1 + n2
 
@@ -25,17 +24,28 @@ operations = {
   '*': multiply,
   '/': divide,
 }
+def calculator():
+  print(logo)
+  num1 = float(input('Enter first number: '))
+  
+  for operation in operations:
+    print(operation)
+  should_continue = True
 
-num1 = int(input('Enter first number: '))
-
-for operation in operations:
-  print(operation)
-
-operation_symbol = input('Select Operation: ')
-
-num2 = int(input('Enter second number: '))
-
-calc_func = operations[operation_symbol]
-answer = calc_func(num1, num2)
-print(f'{num1} {operation_symbol} {num2} = {answer}')
-
+  while should_continue:
+    operation_symbol = input('Select Operation: ')
+    num2 = float(input('Enter next number: '))
+    calc_func = operations[operation_symbol]
+    answer = calc_func(num1, num2)
+    print(f'{num1} {operation_symbol} {num2} = {answer}')
+  
+    while True:
+      another_calc = input('Type "y" to continue with {answer}, or "n" to start a new calculation\n').lower()
+      if another_calc == 'n':
+        should_continue = False
+        calculator()
+        print('Have fun with your calculation. Goodbye.')
+      elif another_calc == 'y':
+        num1 = answer
+      else:
+        print('Type "y" or "n"')
